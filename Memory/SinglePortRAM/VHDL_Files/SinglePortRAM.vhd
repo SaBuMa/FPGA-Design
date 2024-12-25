4be098d40c0dced39385bdc481ed00c205b7a77a
SantiagoBM <84943683+SaBuMa@users.noreply.github.com>
2024-12-25 16:03:47 -0500

    Add files via upload

---
+++ Memory/SinglePortRAM/VHDL_Files/SinglePortRAM.vhd
@@ -0,0 +1,50 @@
+--********************* Single Port RAM *********************--
+--***********************************************************--
+
+--******************* LIBRARY DEFINITION ********************--
+--***********************************************************--
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+
+--***************** ENITY = Inputs Outputs ******************--
+--***********************************************************--
+ENTITY SinglePortRAM IS
+	GENERIC	(	Data_Width				:	INTEGER	:= 8; -- Size of the data to be written in memory
+					Addr_Width				:	INTEGER	:= 2	-- Size of the memory address
+				);	
+	PORT 		(	clk			: 	IN		STD_LOGIC;											-- Clock Input
+					wr_rd_ena	: 	IN		STD_LOGIC;											--	Write/Read Enable
+					addr			: 	IN		STD_LOGIC_VECTOR(Addr_Width-1 DOWNTO 0);  -- Address to write or read
+					Data_write	: 	IN		STD_LOGIC_VECTOR(Data_Width-1 DOWNTO 0);  -- Data to be written
+					Data_read	: 	OUT	STD_LOGIC_VECTOR(Data_Width-1 DOWNTO 0)); -- Data read from memory
+END ENTITY;
+
+ARCHITECTURE rt1 OF SinglePortRAM IS
+
+--******************* Auxiliary cables **********************--
+--***********************************************************--
+
+-- Cables used in "Module Parameterization"
+	TYPE mem_2d IS ARRAY (0 TO 2**Addr_Width-1) OF STD_LOGIC_VECTOR(Data_Width-1 DOWNTO 0);
+
+	SIGNAL ram : mem_2d := (("00000000"),("00000000"),("00000000"),("00000000")); -- Initializing Memory to zeroes
+
+	SIGNAL addr_reg : STD_LOGIC_VECTOR(Addr_Width-1 DOWNTO 0);
+	
+BEGIN
+			
+--************* Parameterized description of RAM ************--
+--***********************************************************--
+WriteProcess: PROCESS (clk)
+	BEGIN
+		IF (rising_edge(clk)) THEN
+			IF (wr_rd_ena = '1') THEN
+				ram(TO_INTEGER(UNSIGNED(Addr))) <= Data_write;
+			END IF;
+			addr_reg <= addr;
+		END IF;
+	END PROCESS;
+	Data_read <= ram(TO_INTEGER(UNSIGNED(addr_reg)));
+
+END ARCHITECTURE;
